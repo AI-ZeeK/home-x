4d5057b5ee9cc280b404fd97f5ccb524e09c5ec0
Isaac Williams <Isaaciyaye174@gmail.com>
2023-09-05 11:35:52 +0100

    Update SupFooter.tsx

--- components/Footer/SupFooter.tsx
+++ components/Footer/SupFooter.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const SupFooter = (props: Props) => {
   return (
-    <div className="pt-10 pb-8 grid grid-cols-1 gap-6 md:grid-cols-[2.5fr_1fr]">
+    <div className="pt-10 pb-8 grid grid-cols-1 gap-6 md:grid-cols-[2.5fr_1fr] overflow-hidden">
       <h1 className="sm:text-[2.8rem] text-[1.8rem] lg:text-[3rem]">
         Looking to buy or sell the property? Homez is here for you!
       </h1>
